644c89f2a74d92276f33705130285ed3f9830581
Andrew Wang <andrewwang39@gmail.com>
2016-10-10 06:00:46 +0000

    update bucketlist

--- BucketList.py
+++ BucketList.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,5 +11,17 @@ def bucket():
 @app.route("/showSignUp")
 def showSignUp():
     return render_template('signup.html')
+@app.route("/signUp", methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate received values
+    if _name and _email and _password:
+	return json.dumps({'html':<span>all fields good</span>})
+    else:
+	return json.dumps({'html':<span>Enter required fields</span>})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
